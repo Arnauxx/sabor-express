cd93c2809069ed42834568f3dfe77c41e55f0eb5
Arnaux <arnauxx@hotmail.com>
2025-05-20 19:51:19 -0300

    Inserindo funções das opções

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,4 +1,7 @@
-print("""
+import os
+
+def exibir_nome_do_programa():
+      print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -7,11 +10,31 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+      print('1. Cadastrar restaurante')
+      print('2. Listar restaurante')
+      print('3. Ativar restaurante')
+      print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+def finalizar_app():
+      os.system('cls')
+      print('Encerrando aplicativo...\n')
 
-print(f'Opção escolhida foi: {opcao_escolhida}')
+def escolher_opcao():
+      opcao_escolhida = int(input('Escolha uma opção: '))
+      if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+      elif opcao_escolhida == 2:
+            print('Listar restaurante')
+      elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+      else:
+            finalizar_app()   
+
+def main():
+      exibir_nome_do_programa()
+      exibir_opcoes()
+      escolher_opcao()         
+
+if __name__ == '__main__':
+      main()
