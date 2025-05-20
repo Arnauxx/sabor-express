29e291a12754d9ded06009ca8d2340b5d13201d3
Arnaux <arnauxx@hotmail.com>
2025-05-20 20:54:26 -0300

    Ajuste da opção de cadastro

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = []
+
 def exibir_nome_do_programa():
       print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -20,18 +22,38 @@ def finalizar_app():
       os.system('cls')
       print('Encerrando aplicativo...\n')
 
+def opcao_invalida():
+      print('Opção inválida!\n')      
+      input('Digite uma tecla para voltar ao menu principal')
+      main()
+
+def cadastrar_novo_restaurante():
+      os.system('cls')
+      print('Cadastro de novos restaurantes\n')
+      nome_do_restaurante = input('Digite o nome do restaurante: ')
+      restaurantes.append(nome_do_restaurante)
+      print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+      input('Digite uma tecla para voltar ao menu principal')
+      main()
+
 def escolher_opcao():
-      opcao_escolhida = int(input('Escolha uma opção: '))
-      if opcao_escolhida == 1:
-            print('Cadastrar restaurante')
-      elif opcao_escolhida == 2:
-            print('Listar restaurante')
-      elif opcao_escolhida == 3:
-            print('Ativar restaurante')
-      else:
-            finalizar_app()   
+      try:
+            opcao_escolhida = int(input('Escolha uma opção: '))
+            if opcao_escolhida == 1:
+                  cadastrar_novo_restaurante()
+            elif opcao_escolhida == 2:
+                  print('Listar restaurante')
+            elif opcao_escolhida == 3:
+                  print('Ativar restaurante')
+            elif opcao_escolhida == 4:
+                  finalizar_app()           
+            else:
+                  opcao_invalida()
+      except:
+            opcao_invalida()                     
 
 def main():
+      os.system('cls')
       exibir_nome_do_programa()
       exibir_opcoes()
       escolher_opcao()         
