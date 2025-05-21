a83524cab5b1bbb856084cd71e81988ddc919158
Arnaux <arnauxx@hotmail.com>
2025-05-21 13:55:00 -0300

    Alterando listas para dicionarios | inclusao do metodo de alterar status e melhoria na visualização

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,6 +1,8 @@
 import os
 
-restaurantes = ['Pizza', 'Sushi']
+restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False}, 
+                {'nome':'Pizza suprema', 'categoria':'Pizzaria', 'ativo':True},
+                {'nome':'Cantina', 'categoria':'Italiano', 'ativo':False}]
 
 def exibir_nome_do_programa():
       print("""
@@ -14,7 +16,11 @@ def exibir_nome_do_programa():
 
 def limpar_tela(texto = ''):
       os.system('cls')
+      linha = '*' * (len(texto))
+      print(linha)
       print(texto)
+      print(linha)
+      print()
       
 def exibe_retorno_menu_principal():
       input('\nDigite uma tecla para voltar ao menu principal ')
@@ -27,26 +33,50 @@ def exibir_opcoes():
       print('4. Sair\n')
 
 def finalizar_app():
-      limpar_tela('Encerrando aplicativo...\n')
+      limpar_tela('Encerrando aplicativo...')
 
-def opcao_invalida():
-      print('Opção inválida!\n')      
+def opcao_invalida(erro = ''):
+      print(f'Opção inválida!\n erro: {erro}')      
       exibe_retorno_menu_principal()
 
 def cadastrar_novo_restaurante():
-      limpar_tela('Cadastro de novos restaurantes\n')
+      limpar_tela('Cadastro de novos restaurantes')
       nome_do_restaurante = input('Digite o nome do restaurante: ')
-      restaurantes.append(nome_do_restaurante)
+      categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
+      dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo': False}
+      restaurantes.append(dados_do_restaurante)
       print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
       exibe_retorno_menu_principal()
 
 def listar_restaurantes():
-      limpar_tela('Lista de restaurantes\n')
+      limpar_tela('Lista de restaurantes')
+
+      print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
+      for restaurante in restaurantes:
+            nome_restaurante = restaurante['nome']
+            categoria = restaurante['categoria']
+            ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+            print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
+
+      exibe_retorno_menu_principal()
+
+def alternar_status_restaurante():
+      limpar_tela('Alterando status do restaurante')
+      nome_restaurante = input('Digite o nome do restaurante que deseja alterar o status: ')
+      restaurante_encontrado = False
 
       for restaurante in restaurantes:
-            print(f'.{restaurante}')
+            if nome_restaurante == restaurante['nome']:
+                  restaurante_encontrado = True
+                  restaurante['ativo'] = not restaurante['ativo']
+                  mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+                  print(mensagem)
 
-      exibe_retorno_menu_principal()          
+      if not restaurante_encontrado:
+            print('O restaurante não foi encontrado')                             
+
+                           
+      exibe_retorno_menu_principal()                
 
 def escolher_opcao():
       try:
@@ -56,13 +86,13 @@ def escolher_opcao():
             elif opcao_escolhida == 2:
                   listar_restaurantes()
             elif opcao_escolhida == 3:
-                  print('Ativar restaurante')
+                  alternar_status_restaurante()
             elif opcao_escolhida == 4:
                   finalizar_app()           
             else:
                   opcao_invalida()
-      except:
-            opcao_invalida()                     
+      except Exception as e:
+            opcao_invalida(str(e))                     
 
 def main():
       limpar_tela()
