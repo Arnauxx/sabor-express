4d04199e9990277b7db372fa56f200cdb5c917c5
Arnaux <arnauxx@hotmail.com>
2025-05-26 21:09:43 -0300

    Adicionando property

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -4,16 +4,20 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome: str = nome
         self.categoria: str = categoria
-        self.ativo: bool = False
+        self._ativo: bool = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria} | {self.ativo}'
+        return f'{self.nome.ljust(25)} | {self.categoria.ljust(25)} | {self.ativo}'
     
     def listar_restaurantes():
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes:
             print(restaurante)
 
+    @property
+    def ativo(self):
+        return '✔️' if self._ativo else '❌'
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
