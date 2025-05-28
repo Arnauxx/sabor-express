2cfebcaf26c513490e71eabca845c0d07136f0e5
Arnaux <arnauxx@hotmail.com>
2025-05-28 19:41:49 -0300

    Alguns ajustes e docstring

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -1,12 +1,14 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-# restaurante_mexicano = Restaurante('mexican food', 'Mexicana')
+restaurante_mexicano = Restaurante('mexican food', 'Mexicana')
 # restaurante_japones = Restaurante('Japa', 'Japonesa')
 
-restaurante_praca.receber_avaliacao('Stevan', 10)
-restaurante_praca.receber_avaliacao('Alice', 8)
-restaurante_praca.receber_avaliacao('Naiala', 5)
+print(restaurante_praca.receber_avaliacao('Stevan', 10))
+print(restaurante_praca.receber_avaliacao('Alice', 0))
+print(restaurante_praca.receber_avaliacao('Naiala', 5))
+print(restaurante_praca.receber_avaliacao('Noah', 1))
+
 
 def main():
     Restaurante.listar_restaurantes()
--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,9 +1,17 @@
 from modelos.avaliacao import Avaliacao
 
 class Restaurante:
+    """Representa um restaurante e suas características."""
     restaurantes = []
 
     def __init__(self, nome, categoria):
+        """
+        Inicializa uma instância de Restaurante.
+
+        Parâmetros:
+        - nome (str): O nome do restaurante.
+        - categoria (str): A categoria do restaurante.
+        """        
         self._nome: str = nome.title()
         self._categoria: str = categoria.upper()
         self._ativo: bool = False
@@ -11,29 +19,45 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
+        """Retorna uma representação em string do restaurante."""        
         return f'{self._nome.ljust(25)} | {self._categoria.ljust(25)} | {str(self.media_avaliacoes).ljust(25)} | {self.ativo}'
     
     @classmethod
     def listar_restaurantes(cls):
+        """Exibe uma lista formatada de todos os restaurantes."""        
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
             print(restaurante)
 
     @property
     def ativo(self):
+        """Retorna um símbolo indicando o estado de atividade do restaurante."""        
         return '✔️' if self._ativo else '❌'
     
     def alternar_estado(self):
+        """Alterna o estado de atividade do restaurante."""
         self._ativo = not self._ativo
 
     def receber_avaliacao(self, cliente, nota):
-        avaliacao = Avaliacao(cliente, nota)
-        self._avaliacao.append(avaliacao)
+        """
+        Registra uma avaliação para o restaurante.
+
+        Parâmetros:
+        - cliente (str): O nome do cliente que fez a avaliação.
+        - nota (float): A nota atribuída ao restaurante (entre 1 e 5).
+        """        
+        if 0 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+            return f'Avaliação incluída com sucesso!!!'
+        else:
+            return f'Valor da nota inválido somente é permitido o valor de 1 a 5'
 
     @property
     def media_avaliacoes(self):
+        """Calcula e retorna a média das avaliações do restaurante."""        
         if not self._avaliacao:
-            return 0
+            return f'-'
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
