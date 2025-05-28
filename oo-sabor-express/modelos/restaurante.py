a146dd61fd079c1d8bbcddf710e877f4c292f66b
Arnaux <arnauxx@hotmail.com>
2025-05-27 21:03:05 -0300

    Calculando e exibindo avaliacoes

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -11,11 +11,11 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self._nome.ljust(25)} | {self._categoria.ljust(25)} | {self.ativo}'
+        return f'{self._nome.ljust(25)} | {self._categoria.ljust(25)} | {str(self.media_avaliacoes).ljust(25)} | {self.ativo}'
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
             print(restaurante)
 
@@ -29,4 +29,13 @@ class Restaurante:
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media        
 
